ef003c3a7d4403310ffabc2da629da4b7596a9b4
nirbhay-py <nnarang1@jpischool.com>
2019-10-17 21:57:53 +0530

    up/down arrow keys functionality added

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 $(document).ready(function(){
+  document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 40) {
+        $("#pswd").focus();
+    }
+    else if(event.keyCode == 38) {
+        $("#username").focus();
+    }
+  });
 })
